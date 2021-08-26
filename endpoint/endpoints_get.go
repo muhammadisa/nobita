e61d27bd7c83006330b1daa45d3ab17df2ee07d5
msawjk@gmail.com <msawjk@gmail.com>
2021-08-27 02:11:29 +0700

    last

--- endpoint/endpoints_get.go
+++ endpoint/endpoints_get.go
@@ -4,8 +4,10 @@ import "github.com/muhammadisa/nobita/middleware"
 
 func (e endpoint) EndpointsGet() {
 	v1 := e.GroupEndpoint()[versionPathV1]
+
 	v1.Use(middleware.JWTAuthMiddleware())
 	v1.Use(middleware.RoleAccessibleMiddleware(e.FeatureRepo))
+
 	v1.GET(e.RouteVersions.RouteFooV1.GetFoo())
 	v1.GET(e.RouteVersions.RouteAuthV1.GetProfile())
 }
